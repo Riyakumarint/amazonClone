d492eddb40bac7ce8c2c373158573b772a63c52e
Riyakumarint <riyakumaribkj@gmail.com>
2020-09-13 23:37:56 +0530

    order

--- src/Order.js
+++ src/Order.js
@@ -24,7 +24,7 @@ function Order( {order} ) {
     ))}
     <CurrencyFormat
         renderText={(value) => (
-            <h3>Order Total: {value}</h3>
+            <h3 className="order__total">Order Total: {value}</h3>
         )}
         decimalScale={2}
         value={order.data.amount /100 }
@@ -36,4 +36,4 @@ function Order( {order} ) {
     )
 }
 
-export default Order;
+export default Order
